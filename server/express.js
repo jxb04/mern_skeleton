88dae7f123a035918c69f91ab6d1930531fe6bb8
Joel Bernasky <joel_bernasky@hotmail.com>
2018-11-23 23:47:27 -0500

    add more auth routes

--- server/express.js
+++ server/express.js
@@ -18,6 +18,11 @@ app.use(helmet())
 app.use(cors())
 app.use('/', userRoutes)
 app.use('/', authRoutes)
+app.use((err, req, res, next) => {
+    if (err.name === 'UnauthorizedError') {
+      res.status(401).json({"error" : err.name + ": " + err.message})
+    }
+  })
 
 app.get('/', (req, res) => {
     res.status(200).send(Template())
